0b4bc429c17e922073df78684710472fbb2a6793
Mike Urezzio <mikeurezzio@gmail.com>
2023-03-23 15:55:28 -0400

    arithmatic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 22;
+let num4 = 6;
 
-// Write your code here
+const multiply = (num1 * num2);
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = (num3 % num4);
+const max = Math.max(1, 2, 3, 4, 20);
